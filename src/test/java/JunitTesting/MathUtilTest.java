bcc9f5b5e05b628d1100c5b692ace6246f6f987f
kitare17 <hoctapak@gmail.com>
2023-07-10 20:34:20 +0700

    met qua huhu

---
+++ src/test/java/JunitTesting/MathUtilTest.java
@@ -0,0 +1,24 @@
+package JunitTesting;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MathUtilTest {
+
+    @Test
+    public void divide() {
+        final int expect =4;
+        final int actual=MathUtil.divide(9,3);
+        Assert.assertEquals(actual,expect);
+
+    }
+
+    @Test
+    public void add() {
+        final int expect =4;
+        final int actual=MathUtil.add(9,3);
+        Assert.assertEquals(actual,expect);
+    }
+}
